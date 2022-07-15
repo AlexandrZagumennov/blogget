299dc53d95cc75491bf053a8c233196031b93986
Alexandr Zagumennov <zagumennov.alex@yandex.ru>
2022-07-15 14:44:51 +0300

    the homework is done

--- src/components/Header/Heading/Heading.jsx
+++ src/components/Header/Heading/Heading.jsx
@@ -1,5 +1,10 @@
 import style from './Heading.module.css';
+import PropTypes from 'prop-types';
 
-export const Heading = ({ content }) => {
-  return <h1 className={style.heading }>{content}</h1>;
-}
+export const Heading = ({content}) => (
+  <h1 className={style.heading }>{content}</h1>
+);
+
+Heading.propTypes = {
+  content: PropTypes.string,
+};
