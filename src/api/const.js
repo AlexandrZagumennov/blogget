9692c8c971badc115a08fcd369a624d03d7a976f
Alexandr Zagumennov <zagumennov.alex@yandex.ru>
2022-07-22 15:46:40 +0300

    Configured to work with the API

---
+++ src/api/const.js
@@ -0,0 +1,7 @@
+export const URL_API = 'https://oauth.reddit.com';
+export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
+export const CLIENT_ID = '7r8_oR3T9Qh8oYP4pjSBmA';
+export const RESPONSE_TYPE = 'token';
+export const RANDOM_STRING = 'random_string';
+export const REDIRECT_URI = 'http://localhost:3000/auth';
+export const SCOPE = 'identity submit read';
