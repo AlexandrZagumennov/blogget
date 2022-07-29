a68275b76c24b654c625ae77ec197c36677dd280
Alexandr Zagumennov <zagumennov.alex@yandex.ru>
2022-07-29 14:25:26 +0300

    initial state

--- src/components/Main/List/Post/Post.jsx
+++ src/components/Main/List/Post/Post.jsx
@@ -8,10 +8,13 @@ import {BtnDelete} from './BtnDelete/BtnDelete';
 import notphoto from '../Post/PostImg/img/notphoto.jpg';
 
 export const Post = ({postData}) => {
+  console.log(postData);
+
   const {
     title,
     author,
     ups,
+    selftext: markdown,
     created,
     thumbnail,
     thumbnail_height: thumbnailHeight,
@@ -24,7 +27,7 @@ export const Post = ({postData}) => {
         postAlt={title}
         src={(thumbnailHeight && thumbnailWidth) ? thumbnail : notphoto}
       />
-      <PostHeading postTitle={title} postAuthor={author}/>
+      <PostHeading postTitle={title} postAuthor={author} markdown={markdown}/>
       <PostRaiting ups={ups}/>
       <PostTime date={created}/>
       <BtnDelete label={'Кнопка для удаления поста'}/>
--- src/components/Main/List/Post/PostHeading/PostHeading.jsx
+++ src/components/Main/List/Post/PostHeading/PostHeading.jsx
@@ -1,35 +1,52 @@
+import {useState} from 'react';
 import style from './PostHeading.module.css';
 import PropTypes from 'prop-types';
 import {Text} from '../../../../UI/Text/Text';
+import {Modal} from '../../../../Modal/Modal';
 
-export const PostHeading = ({postTitle, postAuthor}) => (
-  <div className={style.content}>
-    <Text
-      As='h2'
-      size={20} tsize={22}
-      className={style.title}
+export const PostHeading = ({postTitle, postAuthor, markdown}) => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <div
+      className={style.content}
+      onClick={() => setIsModalOpen(true)}
     >
       <Text
-        As='a'
-        size={26} tsize={32}
-        className={style.linkPost}
-        href='#post'
+        As='h2'
+        size={20} tsize={22}
+        className={style.title}
       >
-        {postTitle}
+        <Text
+          As='a'
+          size={26} tsize={32}
+          className={style.linkPost}
+          href='#post'
+        >
+          {postTitle}
+        </Text>
       </Text>
-    </Text>
 
-    <Text
-      As='a'
-      className={style.linkAuthor}
-      href='#author'
-    >
-      {postAuthor}
-    </Text>
-  </div>
-);
+      <Text
+        As='a'
+        className={style.linkAuthor}
+        href='#author'
+      >
+        {postAuthor}
+      </Text>
+      {isModalOpen && (
+        <Modal
+          title={postTitle}
+          author={postAuthor}
+          markdown={markdown}
+          closeModal={() => setIsModalOpen(false)}
+        />)}
+    </div>
+  );
+};
 
 PostHeading.propTypes = {
   postTitle: PropTypes.string,
   postAuthor: PropTypes.string,
+  markdown: PropTypes.string,
 };
