b0ff4d021b8cb28f24210b9b61fef8aff1c0d4b9
Alexandr Zagumennov <zagumennov.alex@yandex.ru>
2022-08-12 13:07:04 +0300

    change redirect_uri

--- src/api/const.js
+++ src/api/const.js
@@ -3,5 +3,5 @@ export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
 export const CLIENT_ID = '7r8_oR3T9Qh8oYP4pjSBmA';
 export const RESPONSE_TYPE = 'token';
 export const RANDOM_STRING = 'random_string';
-export const REDIRECT_URI = 'http://localhost:3000/auth';
+export const REDIRECT_URI = 'https://blogget-green.vercel.app/auth';
 export const SCOPE = 'identity submit read';
