443fcb484614095ba80c7b3e81b82098c7339e63
Alexandr Zagumennov <zagumennov.alex@yandex.ru>
2022-08-18 10:17:54 +0300

    start page fixed

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,22 +2,21 @@ import style from './Main.module.css';
 import Layout from '../Layout';
 import Tabs from './Tabs';
 import List from './List';
-import {Route, Routes} from 'react-router-dom';
+import {Navigate, Route, Routes} from 'react-router-dom';
 import Modal from '../Modal';
 import Home from './Home';
 import Error from './Error';
-import Start from './Start';
 
 export const Main = () => (
   <main className={style.main}>
     <Layout>
       <Tabs />
       <Routes>
-        <Route path='auth' element={<Home/>} />
-        <Route path='/' element={<Start/>}/>
+        <Route path='/' element={<Home/>}/>
         <Route path='/category/:page' element={<List/>}>
           <Route path='post/:id' element={<Modal/>} />
         </Route>
+        <Route path='auth' element={<Navigate to='/'/>} />
         <Route path='*' element={<Error/>}/>
       </Routes>
     </Layout>
